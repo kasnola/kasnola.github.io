e720ce4450934efffd2e526def5e4e200ffe1cf6
kasnola <keithnolajr@gmail.com>
2024-04-25 18:51:06 -0500

    added runtime

--- projects/billypedia/index.js
+++ projects/billypedia/index.js
@@ -10,24 +10,45 @@ $(document).ready(function() {
         // TODO 3: add style with jQuery
         $('#section-quotes').prependTo('#sections');
         $('#section-quotes').css('background-color', 'grey').css('border-radius', '8px');
-        $('#section-quotes').children().css('padding-left', '10px');
+        $('#section-quotes').children().css('color', 'white').css('padding-left', '10px');
         
         $('.heading-quotes').css('padding-top', '6px');
-        $('.heading-quotes').css('color', 'white')
-        
         $('#quotes').css('padding-bottom', '4px').css('padding-right', '4px');
-        
         $('#section-bio p:last-child').remove();
-
-
-
 
         // uncomment this to inspect all available data; delete when done
         // EXAMPLE: Looping over top rated recordings; replace with your code
-        // let topRated = data.discography.topRated;
-        // _.forEach(topRated, function(recording) {
-        //     console.log(recording);
+        
+        
+        let $list = $('#list-top-rated')
+        let topRated = data.discography.topRated;
+        
+        $list.css('word-wrap', 'normal').width('160px');
+
+        // lost in the sauce
+        // _.forEach(topRated, function(topRated) {
+        //     // console.log(topRated);
+        //     let keys = Object.keys(topRated);
+        //     let $entry = $('<li>').attr('class', 'recording').css('padding-top', '10px')
+        //     let $div = $('<div>').attr('class', 'title')
+
+        //     console.log({keys});
+
+        //     $list.append($entry);
+        //     $entry.append($div.text(topRated.title));
+        //     $entry.append($div.text(topRated.artist));
+        //     $entry.append($div.text(topRated.release));
+        //     $entry.append($div.text(topRated.title));
+
         // });
+        
+        // let $generalRecords = $('<section>').attr('id', 'section-recordings');
+        // let $generalRecordsList = $('<ul>').attr('id', 'list-recordings').attr('class', 'list-recordings');
+        // let recordings = data.discography.recordings
+
+        _.map(topRated, function(topRated) {
+            console.log(topRated.title)
+        });
         
 
         // YOUR CODE ABOVE HERE
